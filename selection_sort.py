579cfee136ddb558c10472a04bb3f2d8882cbac4
Atharva Wagare <52174494+atharva1308@users.noreply.github.com>
2020-06-11 23:20:10 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,4 +1,4 @@
-# Selection Sort
+# Program To Sort Integer Array Using Selection Sort Method
 def sort(arr):
     n=len(arr)
     for i in range(n):
@@ -7,9 +7,9 @@ def sort(arr):
             if arr[j]<arr[min_index]:
                 min_index=j
         arr[i], arr[min_index]=arr[min_index], arr[i]
-    print(arr)
+    print(arr)  # To Print The Sorted Array
 
-# Driver Code
+# Driver Code Below
     
 raw_string=input("Enter Numbers Separated By Commas :: ")
 raw_nums=raw_string.split(",")
@@ -19,4 +19,4 @@ nums=[]
 for i in range(n):
     nums.append(int(raw_nums[i]))
 
-sort(nums)
+sort(nums)  # Function Calling and Passing Integer Array
