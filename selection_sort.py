5f528c2cf0a82cecdd1f3a8b30e42af74f9e614e
Atharva Wagare <52174494+atharva1308@users.noreply.github.com>
2020-06-11 14:03:23 +0530

    Add files via upload

---
+++ selection_sort.py
@@ -0,0 +1,21 @@
+#Selection Sort
+def sort(arr):
+    n=len(arr)
+    print(n)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if arr[j]<arr[min_index]:
+                min_index=j
+        arr[i], arr[min_index]=arr[min_index], arr[i]
+
+    print(arr)
+
+raw_string=input("Enter Numbers Separated By Commas :: ")
+raw_nums=raw_string.split(",")
+n=len(raw_nums)
+nums=[]
+for i in range(n):
+    nums.append(int(raw_nums[i]))
+
+sort(nums)
