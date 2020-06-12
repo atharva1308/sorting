fca8849fd8a72895886a0b75d4a2031a3e2479e4
Atharva Wagare <52174494+atharva1308@users.noreply.github.com>
2020-06-12 23:55:46 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,5 @@
-#Bubble Sort
+
+# Program To Sort Integer Array Using Bubble Sort Method
 def sort(arr):
     n=len(arr)
     for i in range(n):
@@ -6,8 +7,10 @@ def sort(arr):
             if arr[j]>arr[j+1]:
                 arr[j], arr[j+1]=arr[j+1], arr[j]
          
-    print(arr)
+    print(arr)  # To Print The Sorted Array
 
+# Driver Code Below
+    
 raw_string=input("Enter Numbers Separated By Commas :: ")
 raw_nums=raw_string.split(",")
 n=len(raw_nums)
@@ -15,4 +18,4 @@ nums=[]
 for i in range(n):
     nums.append(int(raw_nums[i]))
 
-sort(nums)
+sort(nums)  # Function Calling and Passing Integer Array
